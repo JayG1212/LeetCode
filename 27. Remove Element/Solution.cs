59e9ce4c6256837b165c0a990f1845671629c375
JayG1212 <jaybgunderson@gmail.com>
2024-11-15 21:49:24 -0600

    Update

---
+++ 27. Remove Element/Solution.cs
@@ -0,0 +1,12 @@
+﻿// Written by Jay Gunderson
+// 08/29/2024
+
+namespace _27._Remove_Element;
+
+public class Solution
+{
+     public int RemoveElement(int[] nums, int val) {
+        zazza
+    }
+
+}
