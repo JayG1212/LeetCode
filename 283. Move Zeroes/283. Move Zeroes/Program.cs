ed63b67319a93839a01bbe22c241b411d209650a
JayG1212 <jaybgunderson@gmail.com>
2024-11-16 20:27:30 -0600

    answer

---
+++ 283. Move Zeroes/283. Move Zeroes/Program.cs
@@ -0,0 +1,14 @@
+﻿// Written by Jay Gunderson
+// 11/16/2024
+
+namespace _283._Move_Zeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution aSolution = new Solution();
+
+        }
+    }
+}
---
+++ 283. Move Zeroes/283. Move Zeroes/Solution.cs
@@ -0,0 +1,31 @@
+﻿// Written by Jay Gunderson
+// 11/16/2024
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _283._Move_Zeroes
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+
+            for (int i = 0; i < nums.Count() - 1; i++)
+            {
+                for (int j = 0; j < nums.Count() - 1; j++)
+                {
+                    if (nums[j] == 0)
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
