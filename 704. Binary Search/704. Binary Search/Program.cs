165813d8836ec09622976d0d712656818f68f900
JayG1212 <jaybgunderson@gmail.com>
2024-11-17 15:48:36 -0600

    Update

---
+++ 704. Binary Search/704. Binary Search/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _704._Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
