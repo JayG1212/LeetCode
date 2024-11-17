1cb0fed15e5120dce1aa6e50d0126a61f7dddf60
JayG1212 <jaybgunderson@gmail.com>
2024-11-16 18:56:17 -0600

    Answer

---
+++ 66. Plus One/66. Plus One/Program.cs
@@ -0,0 +1,24 @@
+﻿// Written by Jay Gunderson
+// 11/16/2024
+
+namespace _66._Plus_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution aSolution = new Solution();
+            int[] anArray = {2, 1, 3, 9, 9, };
+            foreach (int i in anArray)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine("\n");
+            int[] answer = aSolution.PlusOne(anArray);
+            foreach(int i in answer)
+            {
+                Console.Write(i + "\t");
+            }
+        }
+    }
+}
---
+++ 66. Plus One/66. Plus One/Solution.cs
@@ -0,0 +1,32 @@
+﻿// Written by Jay Gunderson
+// 11/16/2024
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66._Plus_One
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Count()-1; i >= 0; i--)
+                {
+                if (digits[i] + 1 != 10)
+                {
+                    digits[i] = digits[i] + 1;
+                    return digits;
+                }
+                digits[i] = 0;
+                }
+            int[] newDigits = new int[digits.Count() + 1];
+            newDigits[0] = 1;
+            return newDigits;
+
+        }
+    } 
+}
+
