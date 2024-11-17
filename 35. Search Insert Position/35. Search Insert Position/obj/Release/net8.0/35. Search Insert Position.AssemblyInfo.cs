08fd489b809352a62cd94d99fabe62a8e9c9bdb4
JayG1212 <jaybgunderson@gmail.com>
2024-11-16 18:16:18 -0600

    .

--- 35. Search Insert Position/35. Search Insert Position/obj/Release/net8.0/35. Search Insert Position.AssemblyInfo.cs
+++ 35. Search Insert Position/35. Search Insert Position/obj/Release/net8.0/35. Search Insert Position.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("35. Search Insert Position")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e9ce4c6256837b165c0a990f1845671629c375")]
 [assembly: System.Reflection.AssemblyProductAttribute("35. Search Insert Position")]
