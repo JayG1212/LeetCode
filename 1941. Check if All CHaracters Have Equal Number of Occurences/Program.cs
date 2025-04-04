a62be66c32715cef65800b07831ab45e752bb932
JayG1212 <133731996+JayG1212@users.noreply.github.com>
2025-04-03 19:01:18 -0500

    Answer

---
+++ 1941. Check if All CHaracters Have Equal Number of Occurences/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+
+namespace _1941._Check_if_All_CHaracters_Have_Equal_Number_of_Occurences
+{
+
+    public class Solution
+    {
+        public bool AreOccurrencesEqual(string s)
+        {
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (letters.ContainsKey(c))
+                    letters[c]++;
+                else
+                    letters[c] = 1;
+            }
+            
+
+            HashSet<int> occurences = new HashSet<int>(letters.Values);
+            return occurences.Count == 1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _3._Longest_Substring_Without_Repeating_Characters_
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            HashSet<char> characters = new HashSet<char>();
+            HashSet<int> maxSubString = new HashSet<int>();
+            int currentMax = 0;
+            int max = 0; // Default value so it doesn't return wrong value
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!characters.Contains(s[i]))
+                {
+                    characters.Add(s[i]);
+                    currentMax++;
+                }
+
+            }
+            maxSubString.Add(currentMax);
+
+            foreach (int i in maxSubString)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+
+        }
+
+
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Solution a = new Solution();
+                Console.WriteLine(a.LengthOfLongestSubstring(" "));
+
+            }
+        }
+
+
+    }
+}
